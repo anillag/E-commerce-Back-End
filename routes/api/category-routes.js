c68e6fc21dd1a3a0b80f22547e63166b3ea75c04
Anillag <101154249+anillag@users.noreply.github.com>
2022-07-16 13:40:25 -0400

    Finalized Category API routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,8 +7,10 @@ const db = require("../../config/connection");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  console.log("GET /api/categories  triggered");
-  Category.findAll({})
+  console.log("\nGET /api/categories  triggered\n");
+  Category.findAll({
+    include: [Product],
+  })
     .then((dbData) => {
       return res.json(dbData);
     })
@@ -20,23 +22,24 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // TODO:  be sure to include its associated Products
-  console.log("GET /api/categories/:id  triggered");
+  console.log("\nGET /api/categories/:id  triggered\n");
   Category.findOne({
     where: {
       id: req.params.id,
-    }
-      .then((dbData) => {
-        return res.json(dbData);
-      })
-      .catch((error) => {
-        return res.status(500).json(error.message);
-      }),
-  });
+    },
+    include: [Product],
+  })
+    .then((dbData) => {
+      return res.json(dbData);
+    })
+    .catch((error) => {
+      return res.status(500).json(error.message);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new category
-  console.log("POST /api/categories/  triggered");
+  console.log("\nPOST /api/categories/  triggered\n");
   Category.create({ category_name: req.body.category_name })
     .then((dbData) => {
       return res.json(dbData);
@@ -48,9 +51,8 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
-  console.log("PUT /api/categories/:id  triggered");
+  console.log("\nPUT /api/categories/:id  triggered\n");
   Category.update(
-    req.body,
     {
       category_name: req.body.category_name,
     },
